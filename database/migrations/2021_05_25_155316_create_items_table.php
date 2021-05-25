44bd77fc24e24ac8d607f15da734b684c0d7a7e6
iljadedeiko <lafisrampage123@gmail.com>
2021-05-25 18:55:25 +0300

    DBtables_added

--- database/migrations/2021_05_25_155316_create_items_table.php
+++ database/migrations/2021_05_25_155316_create_items_table.php
@@ -15,7 +15,7 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->integer('category_id');
+            $table->unsignedBigInteger('category_id');
             $table->string('name');
             $table->float('value');
             $table->integer('quality');
